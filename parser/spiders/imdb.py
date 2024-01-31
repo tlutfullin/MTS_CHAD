20254fc989eef0776e042eab01ca4a4959657dd8
tlutfullin <lutfullin2001@inbox.ru>
2024-01-31 19:20:09 +0300

    Исправленный мелкие недочеты

--- parser/spiders/imdb.py
+++ parser/spiders/imdb.py
@@ -9,29 +9,49 @@ class ImdbSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/feature/genre/?ref_=nv_ch_gr']
 
 
+    # Открытие файла для записи заголовков
+    with open('imdb_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['title', 'year', 'imdb_rating', 'genre']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        # Запись заголовков, если файл пуст
+        if csvfile.tell() == 0:
+            writer.writeheader()
+
     def parse(self, response):
         # Извлечение ссылок на страницы с жанрами
         genre_links = response.css('div.ipc-chip-list--base a::attr(href)').getall()
-        for link in genre_links:
+        genre_titles = response.css('.ipc-chip__text::text').getall()
+
+        #print(genre_titles)
+
+        for link, genre_title in zip(genre_links, genre_titles):
             # Проверяем, имеет ли ссылка схему
             if urlparse(link).scheme == '':
                 link = response.urljoin(link)
-            # Отправляем запрос на страницу жанра
-            yield scrapy.Request(url=link, callback=self.parse_movie_details)
+
+            # Создание запроса на вторую страницу с передачей данных через метаданные
+            yield scrapy.Request(url=link, callback=self.parse_movie_details,
+                                 meta={'genre_title':genre_title})
 
     def parse_movie_details(self, response):
+
+        # Извлечение переданных данных из метаданных
+        passed_data = response.meta.get('genre_title')
+        #print('Извлеченные данные',passed_data)
+
         movies = response.css('li.ipc-metadata-list-summary-item')
 
         with open('imdb_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['title', 'year', 'imdb_rating', 'genre']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
+
 
             for movie in movies:
                 title = movie.css('.ipc-title__text::text').get()
                 year = movie.css('.dli-title-metadata-item:nth-child(1)::text').get()
                 imdb_rating = movie.css('.ipc-rating-star.ipc-rating-star--base.ipc-rating-star--imdb.ratingGroup--imdb-rating::text').get()
-                genre = movie.css('.dli-title-type-data::text').get()
+                #genre = movie.css('.dli-title-type-data::text').get()
+                genre = passed_data
 
                 writer.writerow({
                     'title': title,
